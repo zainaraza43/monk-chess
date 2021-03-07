d99ea24c27ca28d7bc986d92b532b6c1c16c6ed0
ghanem025 <worthbooks@hotmail.com>
2021-03-06 20:37:45 -0500

    template

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Minecraft");
+        frame.setSize(1920, 1080);
+        frame.setVisible(true);
+    }
+}
